ce50949b0ff93503df6b83cf76343afd13b783f7
letvarprint <a.brixencov@icloud.com>
2023-10-20 22:00:58 +0300

    Initial Commit

---
+++ hw6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw6
+//
+//  Created by Alexandru Brixencov on 20.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
